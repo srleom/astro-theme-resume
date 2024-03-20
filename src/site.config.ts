eb09d991d9b19a091a7d1656958c7eb1c664f156
srleom <shengrui.leom@gmail.com>
2024-03-20 10:20:46 +0800

    Enhanced branding

--- src/site.config.ts
+++ src/site.config.ts
@@ -3,11 +3,11 @@ import type { AstroExpressiveCodeOptions } from 'astro-expressive-code'
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: 'lorem ipsum',
+	author: 'SRLEOM',
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: 'lorem',
+	title: 'astro-theme-resume',
 	// Meta property used as the default description meta property
-	description: 'The official website of Lorem Ipsum',
+	description: 'The official Astro Resume Theme',
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: 'en-GB',
 	// Meta property, found in src/components/BaseHead.astro L:42
